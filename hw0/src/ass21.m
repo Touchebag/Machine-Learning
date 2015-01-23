db2b33ef805a13823863c5579cd2cb9bef46d6f0
Touchebag <someone90_6@hotmail.com>
2015-01-23 13:42:57 +0100

    Fixed program (I think)

--- hw0/src/ass21.m
+++ hw0/src/ass21.m
@@ -4,4 +4,11 @@
 mu = [1; 1];
 sigma = [0.1 -0.05; -0.05 0.2];
 
+X = mvnrnd(mu, sigma);
+
 f = @(x,r) ((x - mu)' * inv(sigma) * (x - mu)) / 2 - r;
+
+hold on;
+plot1 =  ezplot(@(x1, x2) f([x1:x2], 1));
+set(plot1, 'color', 'r')
+hold off;
