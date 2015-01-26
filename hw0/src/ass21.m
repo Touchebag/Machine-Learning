9c8268497564ab0f4a8321bd66b16a6ad837be10
Touchebag <someone90_6@hotmail.com>
2015-01-26 13:21:14 +0100

    Added more stuff

--- hw0/src/ass21.m
+++ hw0/src/ass21.m
@@ -4,12 +4,18 @@
 mu = [1; 1];
 sigma = [0.1 -0.05; -0.05 0.2];
 
-rng default;
-X = mvnrnd(mu, sigma, 100);
+% rng default;
+% X = mvnrnd(mu, sigma, 100);
 
-f = @(x,r) ((x - mu)' * inv(sigma) * (x - mu)) / 2 - r;
+% x = load('data');
+
+f = @(x,r) (((x - mu)' * inv(sigma) * (x - mu)) / 2) - r;
 
 hold on;
-plot1 =  ezplot(@(X) f(X(:), 1));
+plot1 = ezplot(@(x,y) f([x;y], 1))
+set(plot1, 'color', 'r')
+plot2 = ezplot(@(x,y) f([x;y], 2))
+set(plot1, 'color', 'r')
+plot3 = ezplot(@(x,y) f([x;y], 3))
 set(plot1, 'color', 'r')
 hold off;
