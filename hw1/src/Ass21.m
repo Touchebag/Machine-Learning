eefde45c257db5df6c7cda5f83a725e4e2ccf553
Touchebag <someone90_6@hotmail.com>
2015-02-01 16:56:15 +0100

    Added color in circle function

--- hw1/src/Ass21.m
+++ hw1/src/Ass21.m
@@ -6,13 +6,35 @@ load('dataset1.mat');
 % Calculate mu and sigma
 [mu, sigma] = sge(x);
 
+% Calculate how many points lie outside the circles
+s1 = 0;
+s2 = 0;
+s3 = 0;
+
+for i = 1:size(x, 1)
+  % distance = dist([mu; x(i, :)]);
+  distance = 1;
+
+  if distance > sigma
+    s1 = s1 + 1;
+  end
+  if distance > 2 * sigma
+    s2 = s2 + 1;
+  end
+  if distance > 3 * sigma
+    s3 = s3 + 1;
+  end
+end
+
 % Plot stuff
 hold on;
 
-scatter(x(:,2), x(:,1));
+circle(mu(2) , mu(1) , 1 * sigma , 'r');
+circle(mu(2) , mu(1) , 2 * sigma , 'g');
+circle(mu(2) , mu(1) , 3 * sigma , 'b');
 
-circle(mu(2), mu(1), sigma);
-circle(mu(2), mu(1), 2 * sigma);
-circle(mu(2), mu(1), 3 * sigma);
+legend(sprintf('%d', s1), sprintf('%d', s2), sprintf('%d', s3));
+
+scatter(x(:,2), x(:,1));
 
 hold off;
--- hw1/src/circle.m
+++ hw1/src/circle.m
@@ -1,7 +1,7 @@
 % Helper function to draw a circle
-function circle(x,y,r)
+function circle(x,y,r,c)
   ang=0:0.01:2*pi;
   xp=r*cos(ang);
   yp=r*sin(ang);
-  plot(x+xp,y+yp);
+  plot(x+xp,y+yp, 'Color', c);
 end
