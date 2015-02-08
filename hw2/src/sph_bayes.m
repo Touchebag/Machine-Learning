b1c9becb93d93fcc45b07edbbef251611fb21076
Freddeb <f.bredmar@gmail.com>
2015-02-08 22:55:42 +0100

    sph_bayes minor comments

--- hw2/src/sph_bayes.m
+++ hw2/src/sph_bayes.m
@@ -4,7 +4,7 @@ function [P1, P2, Ytest]=sph_bayes(Xtest, data)
 % Get positions in test data with label 1
 posData = data.y == 1;
 
-% Get x-vectors with label 1
+% Get x-vectors with label 1 and compute mean and std. for each feature.
 xVals1 = data.x(posData, :);
 mu1 = mean(xVals1);
 sigma1 = sqrt(var(xVals1));
@@ -14,11 +14,11 @@ xVals2 = data.x(not(posData), :);
 mu2 = mean(xVals2);
 sigma2 = sqrt(var(xVals2));
 
-% Likelihood for class 1 & 2
+% Likelihood for class 1 & 2. Product of normal pdf of each feature.
 like1 = prod(normpdf(Xtest, mu1, sigma1));
 like2 = prod(normpdf(Xtest, mu2, sigma2));
 
-% Calculate probability for class 1 and 2. Classify xnew with label.
+% Compute probability for class 1 and 2. Classify xnew with label.
 P1 = like1 / (like1 + like2);
 P2 = like2 / (like2 + like1);
 Ytest = (-1)^(P1 < P2);
