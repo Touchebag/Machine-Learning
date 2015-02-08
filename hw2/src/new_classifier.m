e62d94ef8186afa0ae0b35410a39487d816828b9
Freddeb <f.bredmar@gmail.com>
2015-02-08 22:58:15 +0100

    single comment in new_classifier

--- hw2/src/new_classifier.m
+++ hw2/src/new_classifier.m
@@ -1,6 +1,7 @@
 function [Ytest] = new_classifier(Xtest, mu1, mu2)
 
+% Expression shown in lab instructions for problem 2.1 c)
 b = (mu1 + mu2) / 2;
-Ytest = sign((mu1' - mu2')'*(Xtest' - b') / norm(mu1 - mu2));
+Ytest = sign( ( (mu1' - mu2')'*(Xtest' - b') ) / norm(mu1 - mu2));
 
 end
