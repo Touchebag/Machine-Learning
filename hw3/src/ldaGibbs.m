8366d85dfa2f2ec1702949f8a22299b72c12a434
Freddeb <f.bredmar@gmail.com>
2015-02-22 15:41:41 +0100

    implementation draft for 2.1

--- hw3/src/ldaGibbs.m
+++ hw3/src/ldaGibbs.m
@@ -25,11 +25,11 @@
 % -------------------------------------------------------------------
 
 function [beta, theta] = ldaGibbs(data, nTopics, alpha, eta, ...
-    nIt, nReadIt, nBurnIn, instanceWords)
+    nIt, nReadIt, nBurnIn, nWords)
 
 % --- Variable declaration
 nDocuments = length(data);
-beta = zeros(nTopics,instanceWords);
+beta = zeros(nTopics,nWords);
 theta = zeros(nDocuments,nTopics);
 
 % ------- BELOW, YOU SHOULD IMPLEMENT THE GIBBS SAMPLER -------------
@@ -48,28 +48,88 @@ for i = 1:nDocuments
        end
    end
 end
-
 % Initialize z
-z = randi([1,nTopics], length(instanceWords, 1),1);
+z = randi([1,nTopics], instanceWords,1);
+readOutCount = nBurnIn;
 
 % --- Inference, sampling Z
+for iteration = 1:nIt
+    
+    for a = 1:instanceWords 
+       probTopic = zeros(nTopics, 1);
+       for j = 1:nTopics
+          njWi   = 0; % # times word wi got assigned to topic j
+          njAll  = 0; % # words assigned to topic j
+          njDi   = 0; % # words in document di with topic j
+          nDAll  = 0; % # words in document di
 
-for a = 1:instanceWords 
-   maxProb = 0;
-   for j = 1:nTopics
-      njWi   = 0 % # times word wi got assigned to topic j
-      for x = 1:instanceWords
-         if a ~= x & wordArray(a,2) == wordArray(x,2) & z[x] == j
-             njWi = njWi + 1;
-         end
-      end
-      
-      njAll  = 0 % words assigned to topic j
-      njDi   = 0 % words in document di with topic j
-      nDiAll = 0 % words in document di
-      
-   end
+          for x = 1:instanceWords
+             if a ~= x  
+                 if (wordArray(a,2) == wordArray(x,2)) && (z(x) == j)
+                     njWi = njWi + 1;
+                 end
+
+                 if z(x) == j
+                     njAll = njAll + 1;
+                 end
+
+                 if (wordArray(a,1) == wordArray(x,1)) && (z(x) == j)
+                     njDi = njDi + 1;
+                 end
+
+                 if wordArray(a,1) == wordArray(x,1)
+                     nDAll = nDAll + 1;
+                 end
+             end
+          end
+
+          % Expression for probability of topic j for zi
+          probTopic(j,1) = (njWi + eta) / (njAll + nWords*eta) * (njDi + alpha) / (nDAll + nTopics*alpha);
+       end
+       z(a) = randsample(nTopics, 1, true, probTopic);
+    end
+
+    % --- Estimate parameters, beta, theta
+    if readOutCount < 1 || iteration == nIt
+        for k = 1:nWords
+           for l = 1:nTopics
+               nTotTopic = 0;
+               nWordsTopic = 0;
+               
+               for m = 1:instanceWords 
+                  if z(m) ==  l
+                      nTotTopic = nTotTopic + 1;
+                      if wordArray(m, 2) == k
+                          nWordsTopic = nWordsTopic + 1;
+                      end
+                  end
+               end
+               beta(k,l) = (nWordsTopic + eta) / (nTotTopic+nWords*eta);
+           end
+        end
+        
+        for k = 1:nTopics
+            for l = 1:nDocuments
+                nTopicD   = 0;
+                nTotTopic = 0;
+                for m = 1:instanceWords
+                    if wordArray(m,1) == l
+                        nTotTopic = nTotTopic + 1;
+                        if z(m) == k
+                            nTopicD = nTopicD + 1; 
+                        end
+                    end
+                end
+                nTotTopic = nTotTopic + nTopics*alpha;
+                theta(l,k) = (nTopicD + alpha) / nTotTopic;
+            end
+        end
+        iteration % REMOVE BEFORE HAND-IN
+        beta
+        theta
+        readOutCount = nReadIt;
+    else
+        readOutCount = readOutCount - 1;
+    end 
 end
-
-% --- Estimate parameters, beta, theta
 end
