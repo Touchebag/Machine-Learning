bc1b29f79b63a44c62c04989ee561c44731cc47d
Touchebag <someone90_6@hotmail.com>
2015-02-23 12:22:10 +0100

    Fixed inner loop

--- hw3/src/ldaGibbs.m
+++ hw3/src/ldaGibbs.m
@@ -1,17 +1,17 @@
 % -------------------------------------------------------------------
 %
-% This file is part of an exercise in the Machine Learning course of 
+% This file is part of an exercise in the Machine Learning course of
 % Chalmers University of Technology
 %
 % Author: Fredrik Johansson (2013)
 %
 %
 % ldaGibbs(data,nTopics,alpha,eta,nIt,nReadIt,
-%          nBurnIn, nWords) 
-% trains the LDA model based on wordcounts in data using nTopics 
+%          nBurnIn, nWords)
+% trains the LDA model based on wordcounts in data using nTopics
 % topics.
 %
-% data      A cell-array where each element is a sparse representation 
+% data      A cell-array where each element is a sparse representation
 %           of the word count for a single document. Each cell has two
 %           fields, id (representing word ids) and cnt (counts)
 % nTopics   The number of topics to be used
@@ -21,7 +21,7 @@
 % nReadIt   The number of iterations between parameter readouts
 % nBurnIn   The number of iterations making up the burn-in
 % nWords    The number of words in the vocabulary
-% 
+%
 % -------------------------------------------------------------------
 
 function [beta, theta] = ldaGibbs(data, nTopics, alpha, eta, ...
@@ -37,57 +37,59 @@ theta = zeros(nDocuments,nTopics);
 
 
 % --- Initialization
-% number of word instances.
-wordArray  = [];
+number of word instances.
 instanceWords = 0;
 for i = 1:nDocuments
    for j = 1:length(data{i}.id)
-       instanceWords = instanceWords + data{i}.cnt(j);
-       for k = 1:data{i}.cnt(j);            
-          wordArray = [wordArray; [i, data{i}.id(j)]];
-       end
+       instanceWords = instanceWords + 1;
    end
 end
-instanceWords
+
 % Initialize z
-z = randi([1,nTopics], instanceWords,1);
+z = randi([1,nTopics], instanceWords, 1);
 readOutCount = nBurnIn;
 
 % --- Inference, sampling Z
 for iteration = 1:nIt
-    iteration
-    for a = 1:instanceWords 
-       probTopic = zeros(nTopics, 1);
-       for j = 1:nTopics
-          njWi   = 0; % # times word wi got assigned to topic j
-          njAll  = 0; % # words assigned to topic j
-          njDi   = 0; % # words in document di with topic j
-          nDAll  = 0; % # words in document di
+  zIndex = 0;
+    for d = 1:nDocuments
+      for w = 1:length(data{s}.id)
+         zIndex = zIndex + 1;
+         probTopic = zeros(nTopics, 1);
 
-          for x = 1:instanceWords
-             if a ~= x  
-                 if (wordArray(a,2) == wordArray(x,2)) && (z(x) == j)
-                     njWi = njWi + 1;
-                 end
+         for j = 1:nTopics
+            njWi   = 0; % # times word wi got assigned to topic j
+            njAll  = 0; % # words assigned to topic j
+            njDi   = 0; % # words in document di with topic j
+            nDAll  = 0; % # words in document di
 
-                 if z(x) == j
-                     njAll = njAll + 1;
-                 end
+            for di = 1:nDocuments
+              for wi = 1:length(data{di}.id)
+                if data{d}.id{w} ~= data{di}.id{wi}
+                   if (data{d}.id{w} == data{di}.id{wi}) && (z(zIndex) == j)
+                       njWi = njWi + data{di}.cnt{wi};
+                   end
 
-                 if (wordArray(a,1) == wordArray(x,1)) && (z(x) == j)
-                     njDi = njDi + 1;
-                 end
+                   if z(zIndex) == j
+                       njAll = njAll + data{di}.cnt{wi};
+                   end
 
-                 if wordArray(a,1) == wordArray(x,1)
-                     nDAll = nDAll + 1;
-                 end
-             end
-          end
+                   if (d == di) && (z(zIndex) == j)
+                       njDi = njDi + data{di}.cnt{wi};
+                   end
 
-          % Expression for probability of topic j for zi
-          probTopic(j,1) = (njWi + eta) / (njAll + nWords*eta) * (njDi + alpha) / (nDAll + nTopics*alpha);
-       end
-       z(a) = randsample(nTopics, 1, true, probTopic);
+                   if d == di
+                       nDAll = nDAll + data{di}.id{wi};
+                   end
+                end
+              end
+            end
+
+            % Expression for probability of topic j for zi
+            probTopic(j,1) = (njWi + eta) / (njAll + nWords*eta) * (njDi + alpha) / (nDAll + nTopics*alpha);
+         end
+       z(zIndex) = randsample(nTopics, 1, true, probTopic);
+      end
     end
 
     % --- Estimate parameters, beta, theta
@@ -96,8 +98,8 @@ for iteration = 1:nIt
            for l = 1:nTopics
                nTotTopic = 0;
                nWordsTopic = 0;
-               
-               for m = 1:instanceWords 
+
+               for m = 1:instanceWords
                   if z(m) ==  l
                       nTotTopic = nTotTopic + 1;
                       if wordArray(m, 2) == k
@@ -108,7 +110,7 @@ for iteration = 1:nIt
                beta(k,l) = (nWordsTopic + eta) / (nTotTopic+nWords*eta);
            end
         end
-        
+
         for k = 1:nTopics
             for l = 1:nDocuments
                 nTopicD   = 0;
@@ -117,7 +119,7 @@ for iteration = 1:nIt
                     if wordArray(m,1) == l
                         nTotTopic = nTotTopic + 1;
                         if z(m) == k
-                            nTopicD = nTopicD + 1; 
+                            nTopicD = nTopicD + 1;
                         end
                     end
                 end
@@ -131,6 +133,6 @@ for iteration = 1:nIt
         readOutCount = nReadIt;
     else
         readOutCount = readOutCount - 1;
-    end 
+    end
 end
 end
